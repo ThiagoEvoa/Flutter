b3b060b4d4a48759f586ba63aaa05af1c3e09146
Thiago Evoá <thiagoevoa@gmail.com>
2019-03-26 08:21:17 -0300

    Retrieving data from service

--- estudo_flutter/lib/ui/home.dart
+++ estudo_flutter/lib/ui/home.dart
@@ -1,4 +1,5 @@
 import 'package:estudo_flutter/widgets/custom_drawer.dart';
+import 'package:estudo_flutter/widgets/schedule_list.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -8,7 +9,6 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   final _pageController = PageController();
-  List list = ["item1", "item2", "item3"];
 
   @override
   Widget build(BuildContext context) {
@@ -24,45 +24,9 @@ class _HomeState extends State<Home> {
               style: TextStyle(color: Theme.of(context).hintColor),
             ),
           ),
-          body: Scaffold(
-            body: ListView.builder(
-                itemCount: list.length,
-                itemBuilder: (context, index){
-                  return Column(
-                    children: <Widget>[
-                      Padding(
-                        padding: EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 1.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: <Widget>[
-                                Text(list[index]),
-                              ],
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                Text(list[index]),
-                                Padding(padding: EdgeInsets.only(right: 50.0),),
-                                Text(list[index]),
-                              ],
-                            ),
-                            Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: <Widget>[
-                                Text("${list[index]}: ${list[index]}"),
-                                Text("${list[index]}: ${list[index]}")
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                      Divider(color: Theme.of(context).primaryColor,)
-                    ],
-                  );
-                }
+          body: Container(
+            child: Center(
+              child: ScheduleList(),
             ),
           ),
           drawer: CustomDrawer(_pageController),
---
+++ estudo_flutter/lib/util/constants.dart
@@ -0,0 +1,2 @@
+const host = "https://study-node-mongodb.herokuapp.com/api/";
+const url_schedule = "${host}schedule";
