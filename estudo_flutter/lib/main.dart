75b7318a4e39d7a41909b8933fae013df5b9c4ed
Thiago Evoá <thiagoevoa@gmail.com>
2019-03-25 22:00:23 -0300

    Adding schedule list layout

--- estudo_flutter/lib/main.dart
+++ estudo_flutter/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:estudo_flutter/ui/home.dart';
 import 'package:flutter/material.dart';
 import 'ui/login.dart';
 
@@ -8,14 +9,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Study Flutter',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         primaryColor: Colors.blueAccent,
         accentColor: Colors.white30,
         hintColor: Colors.white
       ),
-      home: Login(),
+      home: Home(),
     );
   }
 }
--- estudo_flutter/lib/ui/home.dart
+++ estudo_flutter/lib/ui/home.dart
@@ -8,6 +8,7 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   final _pageController = PageController();
+  List list = ["item1", "item2", "item3"];
 
   @override
   Widget build(BuildContext context) {
@@ -16,12 +17,52 @@ class _HomeState extends State<Home> {
       controller: _pageController,
       children: <Widget>[
         Scaffold(
-          body: AppBar(
+          appBar: AppBar(
             backgroundColor: Theme.of(context).primaryColor,
-            title: Text("Schedules",
-            style: TextStyle(
-                color: Theme.of(context).hintColor
-              ),
+            title: Text(
+              "Schedules",
+              style: TextStyle(color: Theme.of(context).hintColor),
+            ),
+          ),
+          body: Scaffold(
+            body: ListView.builder(
+                itemCount: list.length,
+                itemBuilder: (context, index){
+                  return Column(
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 1.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: <Widget>[
+                                Text(list[index]),
+                              ],
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Text(list[index]),
+                                Padding(padding: EdgeInsets.only(right: 50.0),),
+                                Text(list[index]),
+                              ],
+                            ),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: <Widget>[
+                                Text("${list[index]}: ${list[index]}"),
+                                Text("${list[index]}: ${list[index]}")
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      Divider(color: Theme.of(context).primaryColor,)
+                    ],
+                  );
+                }
             ),
           ),
           drawer: CustomDrawer(_pageController),
